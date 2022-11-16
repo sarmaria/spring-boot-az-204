c350b80209b8d2901ea6ee17d0b601b02d4f9011
charantechie <charantechie@gmail.com>
2022-11-16 22:31:06 +0100

    Updating hello world files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	public String sayHello(){
 		return "Hello Saran";
 	}
